572b6e1917a00876955e99cb901c718890b84912
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-06-05 06:06:09 +0300

    adding and removing active class functionality has been successfully added, finally git add *!

--- js/app.js
+++ js/app.js
@@ -29,10 +29,25 @@ console.log(sections);
  * 
 */
 
-var isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-    return bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth);
-};
+
+
+function elementInViewport(element) {
+    
+    let bounding = element.getBoundingClientRect();
+    let elementHeight = element.offsetHeight;
+    let elementWidth = element.offsetWidth;
+
+    if (bounding.top >= -elementHeight 
+        && bounding.left >= -elementWidth
+        && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + elementWidth
+        && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + elementHeight) {
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 
 /**
@@ -56,7 +71,7 @@ buildNav();
 // Add class 'active' to section when near top of viewport
 function toggleActiveClass(){
     for(let i = 0 ; i < sections.length ; i++){
-        if(isInViewport(sections[i])){
+        if(elementInViewport(sections[i])){
             sections[i].classList.add("active");
         }
         else{
@@ -83,5 +98,3 @@ window.addEventListener("scroll" , function(){
 // Scroll to section on link click
 
 // Set sections as active
-
-
