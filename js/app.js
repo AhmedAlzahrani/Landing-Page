ba07f2f98f4e3e7ed2071d3f3e68463968438d6b
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-06-05 05:39:14 +0300

    scroll event has been added successfully

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
 */
 
 const navbarList = document.getElementById("navbar__list");
-const sections = document.getElementsByTagName("section");
+let sections = document.getElementsByTagName("section");
 const headers = document.getElementsByTagName("h2");
 console.log(sections);
 
@@ -29,6 +29,10 @@ console.log(sections);
  * 
 */
 
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth);
+};
 
 
 /**
@@ -42,15 +46,29 @@ console.log(sections);
 function buildNav(){
     for(let i = 0; i < sections.length ; i++){
         const anchorElement = document.createElement("a");
-        anchorElement.setAttribute("href" , `#${sections[i].id}`);
+        // anchorElement.setAttribute("href" , `#${sections[i].id}`);
+        anchorElement.setAttribute("id" , `#anchor${i}`);
         anchorElement.textContent = headers[i].textContent;
         navbarList.appendChild(anchorElement);
     }
 }
 buildNav();
 // Add class 'active' to section when near top of viewport
+function toggleActiveClass(){
+    for(let i = 0 ; i < sections.length ; i++){
+        if(isInViewport(sections[i])){
+            sections[i].classList.add("active");
+        }
+        else{
+            sections[i].classList.remove("active");
+        }
+    }
+}
 
 
+window.addEventListener("scroll" , function(){
+    toggleActiveClass();
+});
 // Scroll to anchor ID using scrollTO event
 
 
