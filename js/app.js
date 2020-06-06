960bc1862f5765a8ccf4fcb95096767e49a2f181
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-06-06 11:40:59 +0300

    final touches in js/app.js and css/styles.css

--- js/app.js
+++ js/app.js
@@ -19,9 +19,13 @@
 */
 
 const navbarList = document.getElementById("navbar__list");
+
 let sections = document.getElementsByTagName("section");
+
 const headers = document.getElementsByTagName("h2");
-console.log(sections);
+
+const anchorElements = createNavBarElements(sections.length);
+
 
 /**
  * End Global Variables
@@ -34,21 +38,47 @@ console.log(sections);
 function elementInViewport(element) {
     
     let bounding = element.getBoundingClientRect();
+
     let elementHeight = element.offsetHeight;
+    
     let elementWidth = element.offsetWidth;
 
     if (bounding.top >= -elementHeight 
+    
         && bounding.left >= -elementWidth
+    
         && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) + elementWidth
+    
         && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + elementHeight) {
 
-        return true;
+    
+            return true;
+    
     } else {
-        return false;
+            
+            return false;
+
     }
 }
 
+function createNavBarElements(numberOfElements){
+    
+    const anchorElements = [];
 
+    for(let i = 0 ; i < numberOfElements ; i++){
+        const anchorElement = document.createElement("a");
+    
+        // anchorElement.setAttribute("href" , `#${sections[i].id}`);
+    
+        anchorElement.setAttribute("id" , `anchor${i}`);
+    
+        anchorElement.textContent = headers[i].textContent;
+
+        anchorElements.push(anchorElement);
+    }
+
+    return anchorElements;
+}
 
 /**
  * End Helper Functions
@@ -58,24 +88,33 @@ function elementInViewport(element) {
 
 // build the nav
 
-function buildNav(){
-    for(let i = 0; i < sections.length ; i++){
-        const anchorElement = document.createElement("a");
-        // anchorElement.setAttribute("href" , `#${sections[i].id}`);
-        anchorElement.setAttribute("id" , `#anchor${i}`);
-        anchorElement.textContent = headers[i].textContent;
-        navbarList.appendChild(anchorElement);
+function buildNav(elements){
+    
+    for(let i = 0; i < elements.length ; i++){
+    
+        navbarList.appendChild(elements[i]);
+    
     }
 }
-buildNav();
+
+buildNav(anchorElements);
 // Add class 'active' to section when near top of viewport
 function toggleActiveClass(){
+    
     for(let i = 0 ; i < sections.length ; i++){
+    
         if(elementInViewport(sections[i])){
+    
             sections[i].classList.add("active");
+            anchorElements[i].classList.add("active");
+    
         }
+    
         else{
+    
             sections[i].classList.remove("active");
+            anchorElements[i].classList.remove("active");
+    
         }
     }
 }
@@ -93,8 +132,21 @@ function toggleActiveClass(){
 window.addEventListener("scroll" , function(){
     toggleActiveClass();
 });
+
 // Build menu 
 
 // Scroll to section on link click
-
+for (let i = 0 ; i < anchorElements.length ; i ++) {
+    
+    document.querySelector(`#anchor${i}`).addEventListener("click" , function(){
+        
+        window.scrollTo({
+            
+            top: sections[i].offsetTop,
+            
+            behavior: "smooth"
+        
+        });
+    });
+}
 // Set sections as active
